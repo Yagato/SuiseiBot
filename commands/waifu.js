45df70f5c3ffafcfcba48c43c549fb9c9c2f1bb3
Yagato <Yagatomon@gmail.com>
2023-03-23 05:48:09 -0600

    Switched from Giphy to Tenor

--- commands/waifu.js
+++ commands/waifu.js
@@ -1,16 +1,24 @@
-const GphApiClient = require('giphy-js-sdk-core');
 const { SlashCommandBuilder } = require("discord.js")
-const { GIPHY_API_KEY } = require('../config.json');
+const { TENOR_API_KEY } = require('../config.json');
 
-const giphyClient = GphApiClient(GIPHY_API_KEY);
+const Tenor = require("tenorjs").client({
+    "Key": TENOR_API_KEY,
+    "Filter": "off",
+    "Locale": "en_US",
+    "MediaFilter": "minimal",
+    "DateFormat": "D/MM/YYYY - H:mm:ss A"
+});
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('waifu')
     .setDescription('Swiss eggs'),
 
-    async execute(interaction) {
-        const gif = await giphyClient.random('gifs', { tag: 'suisei' });
-        await interaction.reply(gif.data.url);
+    async execute(interaction) {        
+        await Tenor.Search.Random("suisei", "1").then(Results => {
+            Results.forEach(Post => {
+                interaction.reply(Post.url);
+            })
+        }).catch(console.error);
     },
 };
